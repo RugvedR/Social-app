196006b9105cf18eaf9f39751490e67b62ef02d1
RugvedR <rugvedr9@gmail.com>
2022-10-01 14:53:18 +0530

    login and register page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
-// import Home from './pages/home/Home';
-import Profile from './pages/profile/Profile';
+import Home from './pages/home/Home';
 
 function App() {
-  return <Profile/>
+  return <Home/>
 }
 
 export default App;
